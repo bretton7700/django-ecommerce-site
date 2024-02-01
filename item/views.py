fd3eaf19d5984564e48e0c223f30287a031004b1
ngurutheguru <144667249+toptraderke@users.noreply.github.com>
2024-02-01 14:07:24 +0300

    editing items

--- item/views.py
+++ item/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import render,get_object_or_404,redirect
 from .models import Item
-from .forms import NewItemForm
+from .forms import NewItemForm,EditItemForm
 # Create your views here.
 
 def detail(request, pk):
@@ -33,6 +33,7 @@ def new(request):
     return render(request, 'item/form.html', {
         'form': form,
         'title': 'New Item',
+        'button': 'create Item'
     })
 
 @login_required
@@ -40,3 +41,25 @@ def delete(request,pk):
     item = get_object_or_404(Item, pk=pk, created_by=request.user)
     item.delete()
     return redirect('dashboard:dashboard')
+
+
+@login_required
+def edit(request,pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    if not request.user.is_staff:
+        # Add a toast message
+        messages.error(request, "You are not authorized to access that page.")
+        # Redirect to homepage
+        return redirect('/')  
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES,instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit Item',
+        'button' : 'Edit'
+    })

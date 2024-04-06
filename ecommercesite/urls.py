41345914040460ccb96b1037eea3a403cf25de69
bretton7700 <gitongabretton@gmail.com>
2024-04-06 13:13:45 +0300

    static files

--- ecommercesite/urls.py
+++ ecommercesite/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+#from django.conf.urls.static import static
+
+
 
 
 urlpatterns = [
@@ -26,4 +28,4 @@ urlpatterns = [
     path('items/',include('item.urls')),
     path('dashboard/',include('dashboard.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
+] 
